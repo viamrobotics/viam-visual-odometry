b7ef1ec17cd0cf4a88855731130fb7078200b18a
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-08-03 15:59:06 -0400

    clean run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-
-# Be sure to use `exec` so that termination signals reach the python process,
-# or handle forwarding termination signals manually
-# echo "Removing old debug files."
-# rm -r "results"/*
 exec python3 -m src.main $@
