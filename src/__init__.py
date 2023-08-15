843bf93f07aefa5eadf54da7a8555ea82e93c805
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-08-15 09:48:29 -0400

    typo

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,3 @@
-"""
-This files odrive model with the Viam Registry.
-"""
-
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from viam.components.movement_sensor.movement_sensor import MovementSensor
 from .odometry_module import Odometry
