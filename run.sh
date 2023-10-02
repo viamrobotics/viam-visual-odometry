7c7aca4a29d4e8c9cbf96a07a1a82a5e8194d8ad
Bijan Haney <bijan@viam.com>
2023-10-02 17:28:34 -0400

    use the more common way of setting up python modules, and increase the version of the python sdk

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env bash
-
-# bash safe mode. look at `set --help` to see what these are doing
-set -euxo pipefail
-
+#!/bin/sh
 cd $(dirname $0)
-source .env
-./setup.sh
+# Create a virtual environment to run our code
+VENV_NAME="venv"
+PYTHON="$VENV_NAME/bin/python"
+
+python3 -m venv $VENV_NAME
+$PYTHON -m pip install -r requirements.txt 
+#
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
