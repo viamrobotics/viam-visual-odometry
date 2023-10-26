3d9925555869ba0222b0dcc44eac712b7a675e0b
Rob1in <73953872+Rob1in@users.noreply.github.com>
2023-10-25 21:17:45 -0400

    try to fix sleep 3

--- src/visual_odometry.py
+++ src/visual_odometry.py
@@ -202,7 +202,8 @@ class ORBVisualOdometry(object):
             LOGGER.error(f'ITERATION {self.count}')
             LOGGER.error(f"last sleep is : {self.sleep}")
             LOGGER.error(f"time between images  is : {dt}")
-            self.sleep = self.sleep + self.time_between_frames_s-dt
+            # self.sleep = self.sleep + self.time_between_frames_s-dt
+            self.sleep += self.time_between_frames_s-self.sleep
             LOGGER.error(f"sleep is : {self.sleep}")
             
             
